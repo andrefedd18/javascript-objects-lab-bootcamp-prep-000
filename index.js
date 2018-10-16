7abf33dca250f882b858e108efc6714392e98d99
alfedd Fedd <alfedd@crimson.ua.edu>
2018-10-16 05:26:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  var newObject = delete recipes.key
+  var recipes = delete recipes.key
   return recipes
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
